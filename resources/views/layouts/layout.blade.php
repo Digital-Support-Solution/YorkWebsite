e04c228667e007d5d3f0d8cba3824e2a94e4c863
Emmanuel Siziba <emmasiziba@gmail.com>
2024-04-04 14:51:11 +0200

    Update layout.blade.php

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -19,13 +19,13 @@
     <link rel="stylesheet" href="{{ asset('assets/css/animated-headline.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SWK15WT1SG"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y833WZSVYQ"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'G-SWK15WT1SG');
+        gtag('config', 'G-Y833WZSVYQ');
     </script>
 {{--   @vite(["resources/views/*"])--}}
 
